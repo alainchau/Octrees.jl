d59717ee54ba65295c225720d7903f706183c0dc
Alain Chau <phchau@ucsd.edu>
2020-01-08 23:36:03 -0800

    update to v1.3.1

--- src/octree.jl
+++ src/octree.jl
@@ -31,12 +31,12 @@ function needs_refinement(r::MyRefinery, cell)
 end
 
 function refine_data(r::MyRefinery, cell::Cell, indices)
-    newpoints = Int32[]    
-    for i in 1:length(cell.data.ids)
-        if r.X[:, cell.data.ids[i]] in child_boundary(cell, indices)
-            push!(newpoints, cell.data.ids[i])
+    newpoints = Int32[]
+    for i in cell.data.ids
+        if r.X[:, i] in child_boundary(cell, indices)
+            push!(newpoints, i)
         end
-    end        
+    end
     return LeafData(ids = newpoints,
                     code = cell.data.code * (indices .- 1),
                     depth = cell.data.depth + 1)
@@ -124,7 +124,8 @@ const NEIGHBOR_INDICES = (
     Find neighbors with fixed radius r of a given point p using an octree.
     """
 function knn(octree::Octree, p::Vector, r::T) where T<:AbstractFloat
-    ℓ = max(floor(Int, log(2, octree.root.boundary.widths[1] / r)), 1)
+    ℓ = clamp(floor(Int, log(2, octree.root.boundary.widths[1] / r)), 1, octree.maxdepth)
+
     code = LocationalCode(octree::Octree, p)
     neighbors = Set{Int}()
     for (δ1, δ2, δ3) in NEIGHBOR_INDICES
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,33 +2,35 @@ using Octrees
 using Test
 using LinearAlgebra
 using Random
-
 using Plots
 include("../src/misc/plotstuff.jl")
-
-include("../src/misc/helper.jl") #import nearest neighbors
+include("../src/misc/helper.jl")    # import nearest neighbors
 
 @testset "Octrees.jl" begin
     # getindex
     xloc = randstring("01", 10)
     yloc = randstring("01", 10)
     zloc = randstring("01", 10)
+
+    # constructor
     code = LocationalCode(xloc, yloc, zloc)
     @test code[1:5] == LocationalCode(xloc[1:5], yloc[1:5], zloc[1:5])
     @test code[1:end] == LocationalCode(xloc, yloc, zloc)
     @test code[3:end] == LocationalCode(xloc[3:end], yloc[3:end], zloc[3:end])
 
+    # ops
     @test code * [1,0,0] == LocationalCode(xloc * "1", yloc * "0", zloc * "0")
     @test code * "100" == LocationalCode(xloc * "1", yloc * "0", zloc * "0")
 
     # Test locational code functionality
-    N = 100
-    X = randn(3, N)
-    octree = Octree(X)
-    for i in rand(1:N, 10)
-        @test i in code_to_cell(octree, LocationalCode(octree, X[:, i])).data.ids
+    for N in [100, 150, 200]
+        X = randn(3, N)
+        octree = Octree(X)
+        for i in rand(1:N, 10)
+            @test i in code_to_cell(octree, LocationalCode(octree, X[:, i])).data.ids
+        end
     end
-
+    
     # Check if brute force checking for nearest neighbors is equivalent to
     # finding nearest neighbors via octree.
     for iteration in 1:10
@@ -55,9 +57,6 @@ include("../src/misc/helper.jl") #import nearest neighbors
         @test octree_neighbors == actual_neighbors
     end
 
-    
-
-    
     # Plot octree of data projected onto xy-plane.
     X = randn(3,100)
     X[3,:] .= 0
